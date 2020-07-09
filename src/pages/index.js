8d75d5cf9e64702cde429647fbfefad95c876c41
Philip Schönholzer <philip.schoenholzer@apptiva.ch>
2020-07-09 10:51:55 +0200

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { Trans } from "@lingui/react"
+import { Link, withLingui } from "@wapps/gatsby-plugin-lingui"
+
+import Layout from "../components/layout"
+import Image from "../components/image"
+import SEO from "../components/seo"
+
+const IndexPage = () => (
+  <Layout>
+    <SEO title="Home" />
+    <h1>
+      <Trans>Hi people</Trans>
+    </h1>
+    <p>Welcome to your new Gatsby site.</p>
+    <p>Now go build something great.</p>
+    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
+      <Image />
+    </div>
+    <Link to="/page-2/">Go to page 2</Link> <br />
+    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+  </Layout>
+)
+
+export default withLingui()(IndexPage)
+
+export const query = graphql`
+  query($lng: String!, $fallbackLng: String!) {
+    locales: allLocale(
+      filter: { lng: { in: [$lng, $fallbackLng] }, ns: { eq: "messages" } }
+    ) {
+      ...LocaleFragment
+    }
+  }
+`
